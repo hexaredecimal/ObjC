93031e5a492538e05b52d9f73417a21e0d0b239a
hexaredecimal <mfanakagama@gmail.com>
2024-07-24 22:36:33 +0200

    ast: Allow setting the type pointer-ness

--- src/ast/TypeAst.java
+++ src/ast/TypeAst.java
@@ -11,23 +11,31 @@ package ast;
 public class TypeAst extends Ast {
 	private final String id; 
 	private boolean isptr; 
+	private int ptrCount; // Counts the number of "*" in int*
 	
 	public TypeAst(String id) {
 		this.id = id; 
 		this.isptr = false;
+		this.ptrCount = 0;
 	}
 	
 	public TypeAst(String id, boolean isptr) {
 		this.id = id; 
 		this.isptr = isptr;
+		this.ptrCount = 0;
 	}
 
 	public void setIsPtr(boolean isptr) {
 		this.isptr = isptr;
+		this.ptrCount = 1;
+	}
+
+	public void setPtrCount(int count) {
+		this.ptrCount = count;
 	}
 
 	@Override 
 	public String toString() {
-		return this.id.concat(this.isptr ? "*" : "");
+		return this.id.concat(this.isptr ? "*".repeat(ptrCount) : "");
 	}
 }
