c121774902161e777d9cd2759ad1bebb829fd491
hexaredecimal <mfanakagama@gmail.com>
2024-07-24 22:35:46 +0200

    ast: Use TypeAst instead of the runtime types

--- src/ast/ParamAst.java
+++ src/ast/ParamAst.java
@@ -1,16 +1,15 @@
 package ast;
 
-import rtc.Type;
 
 /**
  *
  * @author hexaredecimal
  */
 public class ParamAst extends Ast {
-	private Type type; 
+	private TypeAst type; 
 	private String id; 
 
-	public ParamAst(Type type, String id) {
+	public ParamAst(TypeAst type, String id) {
 		this.type = type; 
 		this.id = id;
 	}
